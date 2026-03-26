c8b82d2269e2a0e18acd8baacf0ed4ac11969e78
wofole11111 <2685045932@qq.com>
2026-03-26 13:18:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,19 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 
-class TestApp(App):
+class MyApp(App):
     def build(self):
         layout = BoxLayout(orientation='vertical', padding=50, spacing=20)
-        
-        title = Label(text='智农慧眼', font_size='40sp')
-        btn = Button(text='点击测试', font_size='30sp')
+        label = Label(text='智农慧眼', font_size='40sp')
+        btn = Button(text='点我测试', font_size='30sp')
         
         def on_press(instance):
-            title.text = '打包成功！'
+            label.text = '打包成功！'
         
         btn.bind(on_press=on_press)
-        
-        layout.add_widget(title)
+        layout.add_widget(label)
         layout.add_widget(btn)
         return layout
 
 if __name__ == '__main__':
-    TestApp().run()
+    MyApp().run()
